256bfd7edebfd217927f5fda1becc51c2e10342a
Tim Rybicki <t.rybicki@gmail.com>
2020-06-28 14:35:58 -0400

    Change header title to Home

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,3 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
@@ -42,7 +35,7 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
+        <Header />
         <Content>
           <main>{children}</main>
           <Footer>
