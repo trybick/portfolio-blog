585befd141f0d491b054afe172b3500de8c8fd74
Tim Rybicki <t.rybicki@gmail.com>
2020-06-28 19:06:36 -0400

    Move GitHub to footer and remove Made With text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import gatsbyLogo from '../images/gatsby-icon.png';
 
 const FooterContainer = styled.footer`
   display: flex;
   justify-content: center;
+`;
+
+const GitHubLink = styled.a`
+  color: black;
+  margin-left: 15px;
+  text-decoration: none;
+  display: inline-block;
+  position: relative;
+
+  ::after {
+    content: '';
+    position: absolute;
+    width: 100%;
+    transform: scaleX(0);
+    height: 2px;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.8);
+    transform-origin: bottom right;
+    transition: transform 0.4s cubic-bezier(0.86, 0, 0.07, 1);
+  }
+
+  :hover::after {
+    transform: scaleX(1);
+    transform-origin: bottom left;
+  }
 `;
 
 const GatsbyLink = styled.a`
@@ -11,13 +38,11 @@ const GatsbyLink = styled.a`
 `;
 
 const Footer = () => (
-  <FooterContainer>
-    <p>
-      © {new Date().getFullYear()}, Built with
-      {` `}
-    </p>
-    <GatsbyLink href="https://www.gatsbyjs.org">Gatsby</GatsbyLink>
-  </FooterContainer>
+  <>
+    <FooterContainer>
+      <GitHubLink href="https://github.com/niklasmtj/gatsby-starter-julia">GitHub</GitHubLink>
+    </FooterContainer>
+  </>
 );
 
 export default Footer;
