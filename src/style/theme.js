18fe088b9daf97ae813522b9df6c1ff5f2912b1b
trybick <t.rybicki@gmail.com>
2024-02-09 14:26:28 -0500

    Fix colors on resume button

--- src/style/theme.js
+++ src/style/theme.js
@@ -1,9 +1,8 @@
 export default {
   colors: {
-    black: '#000',
-    white: '#fff',
     background: '#0A1128',
     // background: 'blue',
     primary: '#f5f5f5',
+    highlight: '#fffa96cc',
   },
 };
