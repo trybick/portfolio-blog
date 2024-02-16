b9bfc8e83ca71f644cd6908a6d64fe4e0d8f76e3
trybick <t.rybicki@gmail.com>
2024-02-15 20:40:15 -0500

    Update acc content

--- src/config/favorites.js
+++ src/config/favorites.js
@@ -16,12 +16,12 @@ export default [
         link: 'https://www.google.com/search?q=better+call+saul&rlz=1C5CHFA_enUS891US891&oq=better+call+saul&aqs=chrome..69i57j46j35i39j0l5.1807j0j7&sourceid=chrome&ie=UTF-8',
       },
       {
-        name: 'Legion',
-        link: 'https://www.google.com/search?q=legion+tv+show&rlz=1C5CHFA_enUS891US891&oq=legion+tv+show&aqs=chrome..69i57j46j0l6.1637j0j7&sourceid=chrome&ie=UTF-8',
+        name: `Scavengers Reign`,
+        link: 'https://www.google.com/search?gs_ssp=eJzj4tVP1zc0LDM0T8suSjMwYPQSKE5OLEvNS08tKlYoSs1MzwMAquIK5w&q=scavengers+reign&oq=scav&gs_lcrp=EgZjaHJvbWUqDAgBEC4YJxiABBiKBTIGCAAQRRg5MgwIARAuGCcYgAQYigUyEggCEAAYQxiDARixAxiABBiKBTINCAMQABiDARixAxiABDINCAQQABiDARixAxiABDINCAUQABiDARixAxiABDIKCAYQABiSAxiABDIHCAcQABiPAjIHCAgQABiPAjIHCAkQABiPAtIBCDI4MTRqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8',
       },
       {
-        name: `Mr. Robot`,
-        link: 'https://www.google.com/search?safe=off&rlz=1C5CHFA_enUS891US891&sxsrf=ALeKk026o-cVh6bje4uNhA_7HxvPzcv7fg%3A1593907798287&ei=VhoBX6SFEZazytMPi-2MgAw&q=mr+robot+2015&oq=mr+robot+2015&gs_lcp=CgZwc3ktYWIQAzICCAAyAggAMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjoECAAQRzoECAAQQzoHCAAQsQMQQzoFCAAQsQNQihpYsR5gpx9oAHABeACAAU2IAeECkgEBNZgBAKABAaoBB2d3cy13aXo&sclient=psy-ab&ved=0ahUKEwikiPyg6bTqAhWWmXIEHYs2A8AQ4dUDCAw&uact=5',
+        name: '1923',
+        link: 'https://www.google.com/search?q=1923&sca_esv=d8934033fdc41234&sxsrf=ACQVn09ZlwmNjPMoAVxtIPvpnnWPXbFvVg%3A1708046180212&ei=ZLfOZc3IDK7JptQP3audwAY&gs_ssp=eJzj4tVP1zc0LDZPK8wyKk43YPRiMbQ0MgYAQoQFhA&oq=1923&gs_lp=Egxnd3Mtd2l6LXNlcnAiBDE5MjMqAggBMgQQIxgnMggQLhixAxiABDIIEAAYgAQYsQMyCBAAGIAEGLEDMgUQABiABDIIEAAYgAQYsQMyCBAAGIAEGLEDMgUQABiABDIIEAAYgAQYsQMyCBAAGIAEGLEDSOkeUK8JWMsQcAN4AZABAJgBbKAB8QKqAQMzLjG4AQPIAQD4AQHCAgoQABhHGNYEGLADwgINEAAYgAQYigUYQxiwA8ICDhAAGOQCGNYEGLAD2AEBwgIWEC4YgAQYigUYQxjUAhjIAxiwA9gBAsICExAuGIAEGIoFGEMYyAMYsAPYAQLCAgsQABiABBiKBRiRAsICERAuGIAEGIoFGJECGMcBGNEDwgILEAAYgAQYsQMYgwHCAg4QLhiABBjHARivARiOBcICERAuGIAEGLEDGIMBGMcBGNEDwgILEC4YgwEYsQMYgATCAhEQABiABBiKBRiRAhixAxiDAcICDhAuGK8BGMcBGIAEGI4FwgIIEC4YgAQYsQPCAg4QLhiDARjUAhixAxiABMICCxAuGK8BGMcBGIAEwgILEC4YgAQYsQMY1ALCAhQQLhiDARivARjHARixAxiABBiOBcICChAAGIAEGIoFGEPiAwQYACBBiAYBkAYTugYGCAEQARgJugYGCAIQARgI&sclient=gws-wiz-serp',
       },
     ],
     icon: BsDisplay,
@@ -38,16 +38,12 @@ export default [
         link: 'https://www.google.com/search?safe=off&rlz=1C5CHFA_enUS891US891&sxsrf=ALeKk004hoE2D4tM6ki3r6rouFGDF45DaQ%3A1593907710427&ei=_hkBX8nVGcuvytMP1fGG8As&q=only+lovers+left+alive+2013&oq=only+lovers+left+alive+2013&gs_lcp=CgZwc3ktYWIQAzICCAAyAggAMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjoECAAQRzoHCAAQFBCHAjoECAAQQ1CJNViQOWCUOmgAcAF4AIABUYgB5QKSAQE1mAEAoAEBqgEHZ3dzLXdpeg&sclient=psy-ab&ved=0ahUKEwjJzIn36LTqAhXLl3IEHdW4Ab4Q4dUDCAw&uact=5',
       },
       {
-        name: 'mid90s',
-        link: 'https://www.google.com/search?q=mid90s+movie&rlz=1C5CHFA_enUS891US891&oq=mid90s+movie&aqs=chrome..69i57j46j0l6.1011j0j9&sourceid=chrome&ie=UTF-8',
-      },
-      {
-        name: 'Roma',
-        link: 'https://www.google.com/search?q=roma+movie&rlz=1C5CHFA_enUS891US891&oq=roma+movie&aqs=chrome..69i57j46j35i39j0l3j69i61j69i60.743j0j9&sourceid=chrome&ie=UTF-8',
-      },
-      {
         name: 'Coherence',
         link: 'https://www.google.com/search?safe=off&rlz=1C5CHFA_enUS891US891&sxsrf=ALeKk01S1cMA_9RlGSO3XPQJncqpFeJvKA%3A1593907866694&ei=mhoBX7qAKqLBytMPkNWK6AE&q=coherence+movie&oq=coherence+movie&gs_lcp=CgZwc3ktYWIQAzIICAAQsQMQkQIyAggAMgUIABCRAjIHCAAQFBCHAjICCAAyAggAMgIIADICCAAyAggAMgIIADoECAAQRzoECCMQJzoFCAAQsQNQ1RRY3hxgqR9oAHABeACAAWaIAfkEkgEDOC4xmAEAoAEBqgEHZ3dzLXdpeg&sclient=psy-ab&ved=0ahUKEwi6tcvB6bTqAhWioHIEHZCqAh0Q4dUDCAw&uact=5',
+      },
+      {
+        name: 'mid90s',
+        link: 'https://www.google.com/search?q=mid90s+movie&rlz=1C5CHFA_enUS891US891&oq=mid90s+movie&aqs=chrome..69i57j46j0l6.1011j0j9&sourceid=chrome&ie=UTF-8',
       },
     ],
     icon: IoIosVideocam,
@@ -56,20 +52,20 @@ export default [
     title: 'Songs',
     items: [
       {
-        name: 'When I Was Done Dying - Dan Deacon',
+        name: 'When I Was Done Dying',
         link: 'https://www.youtube.com/results?search_query=dan+deacon+when+i+was+done+dying',
       },
       {
-        name: 'Weird Fishes - Radiohead',
+        name: 'Weird Fishes',
         link: 'https://www.youtube.com/results?search_query=radiohead+weird+fishes',
       },
       {
-        name: 'I Can Almost See You - Hammock',
-        link: 'https://www.youtube.com/results?search_query=hammock+i+can+almost+see+you',
+        name: 'Heaven Scent',
+        link: 'https://www.youtube.com/watch?v=3rL1kXvfFhU',
       },
       {
-        name: 'Glósóli - Sigur Ros',
-        link: 'https://www.youtube.com/results?search_query=sigur+ros+glosoli',
+        name: 'Ben Böhmer live above Cappadocia in Turkey for Cercle',
+        link: 'https://www.youtube.com/watch?v=RvRhUHTV_8k&t=4s',
       },
     ],
     icon: IoIosMusicalNotes,
@@ -79,7 +75,7 @@ export default [
     items: [
       { name: 'Radiohead', link: 'https://www.youtube.com/results?search_query=radiohead' },
       { name: 'Muse', link: 'https://www.youtube.com/results?search_query=muse' },
-      { name: 'Hammock', link: 'https://www.youtube.com/results?search_query=hammock+band' },
+      { name: 'Martin Sexton', link: 'https://www.youtube.com/results?search_query=martin+sexton' },
       {
         name: 'Above & Beyond',
         link: 'https://www.youtube.com/results?search_query=above+%26+beyond',
@@ -113,8 +109,8 @@ export default [
     title: 'Podcasts',
     items: [
       {
-        name: 'Syntax FM',
-        link: 'https://syntax.fm/',
+        name: 'Lex Fridman Podcast',
+        link: 'https://www.youtube.com/@lexfridman',
       },
       {
         name: 'Darknet Diaries',
@@ -124,6 +120,10 @@ export default [
         name: `Dan Carlin's Hardcore History`,
         link: 'https://www.dancarlin.com/hardcore-history-series/',
       },
+      {
+        name: `Huberman Lab`,
+        link: 'https://www.youtube.com/@hubermanlab',
+      },
     ],
     icon: IoIosMic,
   },
@@ -131,13 +131,10 @@ export default [
     title: 'Quotes',
     items: [
       {
-        name: `"Rules for happiness: something to do, someone to love, something to hope for." - Immanuel Kant`,
+        name: `"Rules for happiness: something to do, someone to love, something to hope for."`,
       },
       {
-        name: `"Don't demand that things happen as you wish, but wish that they happen as they do happen, and you will go on well". - Epictetus`,
-      },
-      {
-        name: `"What happens when we allow everything to be exactly as it is?" - Adyashanti`,
+        name: `"Don't demand that things happen as you wish, but wish that they happen as they do happen, and you will go on well."`,
       },
     ],
     icon: MdFormatQuote,
