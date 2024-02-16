fba86496c1840274321cbba843a736828e6b34af
trybick <t.rybicki@gmail.com>
2024-02-16 10:39:42 -0500

    Revert "Replace typeface nunito with fontsource"
    This reverts commit 42795b9d668a782b8e431191c44abbdf01902554.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import 'prism-themes/themes/prism-vsc-dark-plus.css';
-import '@fontsource-variable/nunito';
+import 'typeface-nunito';
 
 export const wrapRootElement = ({ element }) => {
   return <ChakraProvider>{element}</ChakraProvider>;
