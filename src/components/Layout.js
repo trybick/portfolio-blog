f46f7395712fc6c2d88709a912b8d5a8b85cb4d7
trybick <t.rybicki@gmail.com>
2026-06-28 14:16:56 -0400

    Make footer all the way to bottom

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,21 +4,29 @@ import Header from './Header';
 import Footer from './Footer';
 import '../style/reset.css';
 
+const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
 const Content = styled.div`
+  flex: 1;
   margin: 0 auto;
   max-width: 860px;
+  width: 100%;
   padding: 0 1.0875rem 1rem;
   padding-top: 0;
 `;
 
 const Layout = ({ children }) => (
-  <>
+  <PageWrapper>
     <Header />
     <Content>
       <main>{children}</main>
     </Content>
     <Footer />
-  </>
+  </PageWrapper>
 );
 
 export default Layout;
