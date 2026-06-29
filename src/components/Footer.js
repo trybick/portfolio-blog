f0078fdad5efc89c9f0751eeebb9198f4d969fd3
trybick <t.rybicki@gmail.com>
2026-06-28 20:15:03 -0400

    Add hover to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { Tooltip } from '@chakra-ui/react';
 import { FaEnvelope, FaGithub, FaLinkedin, FaYoutube } from 'react-icons/fa';
 import { FaFileLines } from 'react-icons/fa6';
 import theme from '../@chakra-ui/gatsby-plugin/theme';
 import { resumeHref } from './ViewResumeButton';
 
 const socialLinks = [
-  { label: 'GitHub', href: 'https://github.com/trybick', Icon: FaGithub, size: 22 },
-  { label: 'LinkedIn', href: 'https://www.linkedin.com/in/tim-rybicki/', Icon: FaLinkedin, size: 22 },
-  { label: 'YouTube', href: 'https://www.youtube.com/celtic426', Icon: FaYoutube, size: 22 },
-  { label: 'Resume', href: resumeHref, Icon: FaFileLines, size: 19 },
-  { label: 'Email', href: 'mailto:devtimr@gmail.com', Icon: FaEnvelope, size: 22 },
+  { label: 'GitHub', tooltip: 'GitHub', href: 'https://github.com/trybick', Icon: FaGithub, size: 22 },
+  { label: 'LinkedIn', tooltip: 'LinkedIn', href: 'https://www.linkedin.com/in/tim-rybicki/', Icon: FaLinkedin, size: 22 },
+  { label: 'YouTube', tooltip: 'YouTube', href: 'https://www.youtube.com/celtic426', Icon: FaYoutube, size: 22 },
+  { label: 'Resume', tooltip: 'View resume', href: resumeHref, Icon: FaFileLines, size: 19 },
+  { label: 'Email', tooltip: 'Email me', href: 'mailto:devtimr@gmail.com', Icon: FaEnvelope, size: 22 },
 ];
 
 const FooterContainer = styled.footer`
@@ -37,10 +38,12 @@ const IconLink = styled.a`
 const Footer = () => (
   <FooterContainer>
     <IconRow>
-      {socialLinks.map(({ label, href, Icon, size }) => (
-        <IconLink key={label} href={href} target="_blank" rel="noreferrer" aria-label={label}>
-          <Icon size={size} />
-        </IconLink>
+      {socialLinks.map(({ label, tooltip, href, Icon, size }) => (
+        <Tooltip key={label} label={tooltip} hasArrow openDelay={100}>
+          <IconLink href={href} target="_blank" rel="noreferrer" aria-label={label}>
+            <Icon size={size} />
+          </IconLink>
+        </Tooltip>
       ))}
     </IconRow>
   </FooterContainer>
